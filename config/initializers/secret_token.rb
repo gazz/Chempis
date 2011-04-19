49a2c63fa29af232a016a4d0e6b1715e965e15f9
gazz <gazz@twizt.com>
2011-04-19 15:42:20 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gonka::Application.config.secret_token = 'e54046c37b0a72e0e57abb5d6fdc748526a3a8e144226996e79f419b58c3fbb53798b462b13b7a1afb342a059db02fca7efac98354975ce80a90593d5bc083e3'
